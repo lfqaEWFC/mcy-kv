09d96bbeaba0f1df4aab95c0e06f0e875cc0cefd
lfqaEWFC <212128641@qq.com>
2026-03-05 20:50:45 +0800

    client(put)-test

---
+++ main-client/client.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"mcy-kv/kvclient"
+)
+
+func main() {
+
+	servers := map[int]string{
+		0: "127.0.0.1:8001",
+		1: "127.0.0.1:8002",
+		2: "127.0.0.1:8003",
+		3: "127.0.0.1:8004",
+		4: "127.0.0.1:8005",
+	}
+
+	ck := kvclient.MakeClerk(servers)
+
+	ck.Put("x", "1")
+	ck.Put("x", "2")
+}
